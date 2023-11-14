c800f7a949dd404c2f8378e8a5e6819c41190e8d
Rodolfo <carmen.rodolfo2003@gmail.com>
2023-11-14 02:30:27 -0600

    Add project files.

---
+++ LegalAlcoholAgeCalc/LegalAlcoholAgeCalc/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace LegalAlcoholAgeCalc
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            double LegalAge = DateTime.Now.Year - DateOfBirth.Date.Year;
+            Out_Years.Text = LegalAge.ToString();
+            double YearsLeft = LegalAge - 21;
+            if (LegalAge >= 21)
+            {
+                Out_Years.Text = "You Are " + LegalAge + " So you are Legally able to Drink";
+            }
+            else
+            {
+                Out_Years.Text = "You Still need " + YearsLeft + " years left  To be able to leaglly drink" ;
+
+            }
+        }
+    }
+}
